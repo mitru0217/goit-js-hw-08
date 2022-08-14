131431986c25bd4c51f891056c65892ef05bf178
Michael <mitru0217@gmail.com>
2022-08-14 18:50:55 +0300

    hw-js-08
    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,16 @@
-// import Player from '@vimeo/player';
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+function setLocaleStorage({ seconds }) {
+   localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+};
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title)});
+const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(currentTime || 0);
+
+
+player.on("timeupdate", throttle(setLocaleStorage, 500)); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+const STORAGE_KEY = `feedback-form-state`;
+
+const formData = {};
+
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    textarea: document.querySelector(".feedback-form textarea"),
+    input: document.querySelector(".feedback-form input"),
+};
+
+ saveLocaleStorage ();
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.form.addEventListener("input", e => {
+    formData[e.target.name] = e.target.value;
+   
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+
+});
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    if(refs.textarea.value === "" || refs.input.value === "") {
+        return alert("Please fill all fields");
+    }
+    e.currentTarget.reset();
+   localStorage.removeItem(STORAGE_KEY); 
+};
+
+function saveLocaleStorage () {
+    const savedInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
+   
+    if(savedInfo) {
+        for(const key in savedInfo) {
+            if(savedInfo.hasOwnProperty(key)) {
+                refs.textarea.value = savedInfo.message || "";
+                refs.input.value = savedInfo.email || "";
+            }
+        }
+      }
+    }
+
 
